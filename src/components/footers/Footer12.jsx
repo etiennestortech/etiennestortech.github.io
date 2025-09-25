1718bda1c1042e9d409090a080e4baef38ab7cbe
larucheweb <info@larucheweb.com>
2025-09-24 20:11:53 -0400

    Update footer: remove circular images, add Facebook and Instagram links, keep YouTube without link

--- src/components/footers/Footer12.jsx
+++ src/components/footers/Footer12.jsx
@@ -13,17 +13,6 @@ const InstagramIcon = () => (
   </svg>
 );
 
-const TwitterIcon = () => (
-  <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
-    <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"/>
-  </svg>
-);
-
-const LinkedInIcon = () => (
-  <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
-    <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/>
-  </svg>
-);
 
 const YouTubeIcon = () => (
   <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
@@ -59,7 +48,7 @@ type Props = {
   columnLinks: ColumnLinks[];
   socialMediaLinks: SocialMediaLinks[];
   footerText?: string;
-  footerImages: ImageProps[];
+  footerLinks?: Links[];
 };
 
 export type Footer12Props = React.ComponentPropsWithoutRef<"section"> & Partial<Props>;
@@ -72,7 +61,6 @@ export const Footer12 = (props: Footer12Props) => {
     columnLinks,
     socialMediaLinks,
     footerText,
-    footerImages,
     buttons,
   } = {
     ...Footer12Defaults,
@@ -112,16 +100,6 @@ export const Footer12 = (props: Footer12Props) => {
             <a href={logo.url}>
               <img src={logo.src} alt={logo.alt} className="mb-6 inline-block sm:mb-0" style={{maxWidth: '9rem'}} />
             </a>
-            <div className="ml-3 flex">
-              {footerImages.map((image, index) => (
-                <img
-                  key={index}
-                  src={image.src}
-                  alt={image.alt}
-                  className="relative -ml-3 w-12 h-12 min-h-12 min-w-12 rounded-full border-2 border-white object-cover "
-                />
-              ))}
-            </div>
           </div>
         </div>
         <div className="flex flex-col-reverse items-start justify-between pb-4 pt-6 text-sm md:flex-row md:items-center md:pb-0 md:pt-8">
@@ -170,33 +148,9 @@ export const Footer12Defaults: Props = {
     },
   ],
   socialMediaLinks: [
-    { url: "#", icon: <FacebookIcon /> },
-    { url: "#", icon: <InstagramIcon /> },
-    { url: "#", icon: <TwitterIcon /> },
-    { url: "#", icon: <LinkedInIcon /> },
+    { url: "https://www.facebook.com/people/Tight-Compact-Storage/61574985541826/", icon: <FacebookIcon /> },
+    { url: "https://www.instagram.com/tightandcompact/", icon: <InstagramIcon /> },
     { url: "#", icon: <YouTubeIcon /> },
   ],
   footerText: "© 2024 Relume. All rights reserved.",
-  footerImages: [
-    {
-      src: "https://d22po4pjz3o32e.cloudfront.net/placeholder-image-small.svg",
-      alt: "Relume placeholder image",
-    },
-    {
-      src: "https://d22po4pjz3o32e.cloudfront.net/placeholder-image-small.svg",
-      alt: "Relume placeholder image",
-    },
-    {
-      src: "https://d22po4pjz3o32e.cloudfront.net/placeholder-image-small.svg",
-      alt: "Relume placeholder image",
-    },
-    {
-      src: "https://d22po4pjz3o32e.cloudfront.net/placeholder-image-small.svg",
-      alt: "Relume placeholder image",
-    },
-    {
-      src: "https://d22po4pjz3o32e.cloudfront.net/placeholder-image-small.svg",
-      alt: "Relume placeholder image",
-    },
-  ],
 };
